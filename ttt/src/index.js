16632dd0513266c96d98906e40916d5527fdfdaa
Jun Alvarado <jun3rd@gmail.com>
2020-10-26 12:35:22 -0700

    created starter files w/o help

--- ttt/src/index.js
+++ ttt/src/index.js
@@ -2,11 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Square extends React.Component {
+    render() {
+        return (
+            <div className="square">
+                {/* this.props.value */}
+            </div>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return (
+            <Square />
+        ); {/* <Square value={i}/> */}
+    }
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
 class Game extends React.Component {
     render() {
         return (
-            <div>
-                hello
+            <div className="game">
+                <div>
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* ToDo: show status */}</div>
+                    <ol>{/* ToDo */}</ol>
+                </div>
             </div>
         )
     }
